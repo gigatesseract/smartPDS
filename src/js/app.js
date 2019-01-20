3acf54a45733928adf931520356adb763517bd42
Kamalesh Palanisamy <kamalesh800@gmail.com>
2019-01-20 12:41:25 +0530

    Nannum commit paneten

--- src/js/app.js
+++ src/js/app.js
@@ -41,14 +41,16 @@ $.getJSON("https://jsonip.com/?callback=?", data => {
 
 function addPDS() {
   uin = $("#f_aadhar").val();
+  uin = uin.replace(/\s/g, '');
   _type = $("#_type").val();
   weight = $("#weight").val();
 
   quality = $("#quality").val();
   q_aadhar = $("#q_aadhar").val();
+  q_aadhar = q_aadhar.replace(/\s/g, '');
   var date = new Date();
   var time = date.getTime();
-  time.toString();
+  time = time.toString();
   Mycontract.deployed().then(function(instance) {
     instance.addPDS(
       time,
@@ -83,7 +85,7 @@ function pdsDisplay(pdsarray) {
   if (pdsarray.length > 0) {
     table_string = "";
     table_string +=
-      "<table class = 'table'><tr><th>UIN</th><th>Type of goods</th><th>Quality</th><th>Weight(Kg)</th><th>Location</th><th>Arrival</th>";
+      "<table class='table'><tr><th>UIN</th><th>Type of goods</th><th>Quality Checker Number</th><th>Quality</th><th>Weight(Kg)</th><th>Location</th><th>Arrival</th>";
     pdsarray.forEach(function(element) {
       table_string +=
         "<tr><td>" +
@@ -91,8 +93,10 @@ function pdsDisplay(pdsarray) {
         "</td><td>" +
         element[2] +
         "</td><td>" +
-        element[3] +
+        element[3].slice(2,) +
         "</td><td>" +
+        element[3].slice(0,2)+
+        "</td><td>"+
         element[4] +
         "</td><td>" +
         element[5] +
@@ -102,6 +106,8 @@ function pdsDisplay(pdsarray) {
     });
   }
   $("#table").html(table_string);
+  $('.table').css('margin','0');
+  $('.table').css("width","100%");
 };
 function hex_to_ascii(str1)
 {
